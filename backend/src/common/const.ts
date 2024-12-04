da09862a13321850df32a17c5baa7f533df02939
shivi30 <shivani.sharma1@appinventiv.com>
2024-12-04 17:43:29 +0530

    testing for rebase done

--- backend/src/common/const.ts
+++ backend/src/common/const.ts
@@ -5,5 +5,6 @@ export const CONST = {
     FORGOT_PASSWORD: "Forgot Password",
     EMAIL_TEMPLATES: `${process.cwd()}/src/mailer/forgotPassword.html`,
     MONGODB_OBJECTID_REGEX: /^[0-9a-fA-F]{24}$/,
+    REBASE: true
 
 }
